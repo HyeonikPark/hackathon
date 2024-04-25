6b466c98fdcd835d7855956716958f497c92f11a
손성민 <alexzzanga7407@gmail.com>
2024-04-25 16:49:49 +0900

    init

---
+++ src/test/java/com/app/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
